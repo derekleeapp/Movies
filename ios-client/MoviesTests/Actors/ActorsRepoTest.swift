734d767268b7d485d20267f984edc36835deda14
Derek Boire <dboire@pivotal.io>
2016-03-15 15:51:26 +0900

    Remove now duplicated test.
    Signed-off-by: Kyle Gearhart <kgearhart@pivotal.io>

--- ios-client/MoviesTests/Actors/ActorsRepoTest.swift
+++ ios-client/MoviesTests/Actors/ActorsRepoTest.swift
@@ -6,8 +6,8 @@ class ActorsRepoTest: XCTestCase {
 
     func testActorsRepo_hitsCorrectEndpoint() {
         let fakeHttp = FakeHttp()
-
         let actorsRepo = ActorsRepo(http: fakeHttp)
+
         actorsRepo.getAll()
 
         XCTAssertEqual(fakeHttp.get_args, "/actors")
@@ -15,7 +15,6 @@ class ActorsRepoTest: XCTestCase {
 
     func testActorsRepo_returnsActorName() {
         let fakeHttp = FakeHttp()
-
         let actorsRepo = ActorsRepo(http: fakeHttp)
 
         let promise = Promise<NSData, HttpError>()
@@ -35,29 +34,5 @@ class ActorsRepoTest: XCTestCase {
         waitForExpectationsWithTimeout(0.01, handler: nil)
 
         XCTAssertEqual(actualActorName, "Joseph")
-    }
-
-    func testActorsRepo_returnsDifferentActorName() {
-        let fakeHttp = FakeHttp()
-
-        let actorsRepo = ActorsRepo(http: fakeHttp)
-
-        let promise = Promise<NSData, HttpError>()
-        fakeHttp.get_returnValue = promise.future
-
-        let testExpectation = expectationWithDescription("")
-
-
-        var actualActorName = ""
-        actorsRepo.getAll()
-            .onSuccess { value in
-                actualActorName = value
-                testExpectation.fulfill()
-        }
-
-        promise.success("Joe".dataUsingEncoding(NSUTF8StringEncoding)!)
-        waitForExpectationsWithTimeout(0.01, handler: nil)
-
-        XCTAssertEqual(actualActorName, "Joe")
     }
 }
